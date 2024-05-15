6612d73baf19ad86869a9a8e494d11f0bfac84cb
darren thiorres <darrenthiores@gmail.com>
2024-05-15 21:47:12 +0700

    + add jab cross

--- Privcise/ViewController.swift
+++ Privcise/ViewController.swift
@@ -10,7 +10,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -23,10 +23,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let scene = SCNScene(named: "art.scnassets/BackSquatNew.dae")!
+//        let nodeArray = scene.rootNode.childNodes
+//
+//        for childNode in nodeArray {
+//            childNode.position = SCNVector3(x: 0, y: 0, z: -100)
+//            childNode.scale = SCNVector3(0.001, 0.001, 0.001)
+//
+//            sceneView.scene.rootNode.addChildNode(childNode)
+//        }
         
         // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,9 +42,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        // Create an anchor at the world origin
+        let anchor = ARAnchor(name: "backSquatAnchor", transform: matrix_identity_float4x4)
+        sceneView.session.add(anchor: anchor)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -45,17 +58,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
+     return node
+     }
+     */
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if anchor.name == "backSquatAnchor" {
+//            // Create a new scene
+//            let zombieScene = SCNScene(named: "art.scnassets/Zombie.scn")!
+//            if let zombieNode = zombieScene.rootNode.childNode(withName: "scene", recursively: true) {
+//                // Set the position relative to the anchor (which is at the origin)
+//                zombieNode.position = SCNVector3(x: 0, y: 0, z: -10)
+//
+//                // Add the zombieNode to the anchor's node
+//                node.addChildNode(zombieNode)
+//
+//                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: -0.5), duration: 10.0)
+//                zombieNode.runAction(moveAction)
+//            }
+            let scene = SCNScene(named: "art.scnassets/JabCross.scn")!
+//            let nodeArray = scene.rootNode.childNodes
+//
+//            for childNode in nodeArray {
+//                childNode.position = SCNVector3(x: 0, y: 0, z: -10)
+//
+//                node.addChildNode(childNode)
+//
+//                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: -0.5), duration: 10.0)
+//                childNode.runAction(moveAction)
+//            }
+            if let childNode = scene.rootNode.childNode(withName: "scene", recursively: true) {
+                // Set the position relative to the anchor (which is at the origin)
+                childNode.position = SCNVector3(x: 0, y: 0, z: -10)
+                
+                // Add the zombieNode to the anchor's node
+                node.addChildNode(childNode)
+                
+                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: -0.5), duration: 10.0)
+                childNode.runAction(moveAction)
+            }
+        }
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
